a7ae957cca1f9a98061d6df4f69e1b45a1be2c82
Mohammed Salman <mohammad.salman@neosoftmail.com>
2025-06-23 23:07:54 +0530

    feat: api base url added

--- logger.js
+++ logger.js
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
   level: 'info',
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'app.log' })
+    new winston.transports.File({ filename: './logs/app.log' })
   ],
   format: winston.format.combine(winston.format.timestamp(), winston.format.simple())
 });
--- services/weatherService.js
+++ services/weatherService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { getApiKey } from './apiKeyService.js';
+const { OPENWEATHER_BASE_URL, MAPBOX_BASE_URL } = process.env;
 
 export async function getCoordinates(address) {
   const mapboxKey = await getApiKey('mapbox');
   if (!mapboxKey) throw new Error('Mapbox API key not found.');
-
-  const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxKey}&limit=1`;
+  const geoUrl = `${MAPBOX_BASE_URL}/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxKey}&limit=1`;
   const geoRes = await axios.get(geoUrl);
   const features = geoRes.data.features;
 
@@ -19,7 +19,7 @@ export async function getWeather(lat, lon) {
   const weatherKey = await getApiKey('openweathermap');
   if (!weatherKey) throw new Error('OpenWeather API key not found.');
 
-  const url = `https://api.openweathermap.org/data/3.0/onecall?appid=${weatherKey}&exclude=daily,minutely&units=metric&lat=${lat}&lon=${lon}`;
+  const url = `${OPENWEATHER_BASE_URL}/data/3.0/onecall?appid=${weatherKey}&exclude=daily,minutely&units=metric&lat=${lat}&lon=${lon}`;
   const weatherRes = await axios.get(url);
   return weatherRes.data;
 }
