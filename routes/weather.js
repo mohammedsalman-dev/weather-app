6c5f343537f4af414f289dc6dd72020f14d1bc53
Mohammed Salman <mohammad.salman@neosoftmail.com>
2025-06-23 17:14:34 +0530

    feat: db connection established and test db connection API created

--- routes/weather.js
+++ routes/weather.js
@@ -1,11 +1,24 @@
 // routes/weather.js
 const express = require('express');
 const router = express.Router();
+const db = require('../db/connection');
+
 
 router.get('/', (req, res) => {
 });
 
 router.post('/weather', (req, res) => {
+});
+
+
+router.get('/test-db', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM weather_history');
+    res.json(rows);
+  } catch (error) {
+    console.error('DB error:', error);
+    res.status(500).json({ error: 'Database error' });
+  }
 });
 
 module.exports = router;
